1fcdfc532c4515bcf16962a4cbe9119aed6bd61a
Ledenevski <Ledenevski@DESKTOP-1MKH773>
2022-04-19 10:50:04 +0300

    first commit

---
+++ src/sample/Main.java
@@ -0,0 +1,20 @@
+package sample;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Triangle triangle1 = new Triangle(3, 5, 6);
+		
+		System.out.println(triangle1.getArea());
+		
+		Triangle triangle2 = new Triangle(8, 7, 3);
+		
+		System.out.println(triangle2.getArea());
+		
+		Triangle triangle3 = new Triangle(5, 5, 5);
+		
+		System.out.println(triangle3.getArea());
+	}
+
+}
